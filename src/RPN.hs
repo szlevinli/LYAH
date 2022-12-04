5cbfdee09d8d7171d8218576c1c0aa381593b1b4
levin li <3259963@qq.com>
2022-12-04 11:23:56 +0800

    RPN

---
+++ src/RPN.hs
@@ -0,0 +1,15 @@
+module RPN (solveRPN) where
+
+import Data.List ()
+
+solveRPN :: String -> Float
+solveRPN = head . foldl foldingFunction [] . words
+  where
+    foldingFunction (x : y : ys) "*" = (x * y) : ys
+    foldingFunction (x : y : ys) "+" = (x + y) : ys
+    foldingFunction (x : y : ys) "-" = (y - x) : ys
+    foldingFunction (x : y : ys) "/" = (y / x) : ys
+    foldingFunction (x : y : ys) "^" = (y ** x) : ys
+    foldingFunction (x : ys) "ln" = log x : ys
+    foldingFunction xs "sum" = [sum xs]
+    foldingFunction xs numberString = read numberString : xs
