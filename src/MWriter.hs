0aa943e20a6ebcd80b1618d2f1f3d533c7c6d036
levin li <3259963@qq.com>
2023-01-08 11:03:24 +0800

    implement MWriter

--- src/MWriter.hs
+++ src/MWriter.hs
@@ -1,4 +1,11 @@
-module MWriter (multWithLog, gcd') where
+{-# LANGUAGE InstanceSigs #-}
+
+module MWriter
+  ( multWithLog,
+    gcd',
+    runMWriter,
+  )
+where
 
 import Control.Monad.Writer (Writer, tell, writer)
 
@@ -20,3 +27,25 @@ gcd' a b
   | otherwise = do
       tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
       gcd' b (a `mod` b)
+
+newtype MWriter w a = MWriter {runMWriter :: (a, w)}
+
+instance (Monoid w) => Functor (MWriter w) where
+  fmap :: (a -> b) -> MWriter w a -> MWriter w b
+  fmap f (MWriter (a, w)) = MWriter (f a, w)
+
+instance (Monoid w) => Applicative (MWriter w) where
+  pure :: a -> MWriter w a
+  pure a = MWriter (a, mempty)
+
+  (<*>) :: MWriter w (a -> b) -> MWriter w a -> MWriter w b
+  MWriter (f, w) <*> MWriter (a, w') = MWriter (f a, w `mappend` w')
+
+instance (Monoid w) => Monad (MWriter w) where
+  return :: a -> MWriter w a
+  return = pure
+
+  (>>=) :: MWriter w a -> (a -> MWriter w b) -> MWriter w b
+  MWriter (a, w) >>= f =
+    let MWriter (b, w') = f a
+     in MWriter (b, w <> w')
