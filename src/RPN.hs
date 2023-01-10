f5a11d01db4ca8cfc10602a52e77f35ebb962877
levin li <3259963@qq.com>
2023-01-10 21:14:01 +0800

    add inner function signature

--- src/RPN.hs
+++ src/RPN.hs
@@ -5,6 +5,7 @@ import Data.List ()
 solveRPN :: String -> Float
 solveRPN = head . foldl foldingFunction [] . words
   where
+    foldingFunction :: [Float] -> String -> [Float]
     foldingFunction (x : y : ys) "*" = (x * y) : ys
     foldingFunction (x : y : ys) "+" = (x + y) : ys
     foldingFunction (x : y : ys) "-" = (y - x) : ys
